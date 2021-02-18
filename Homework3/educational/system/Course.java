c786194eedf478cda90112115de2d9e5fbb39f0e
Rasim <Rasim467@mail.ru>
2021-02-18 14:57:55 +0400

    add Homework3

---
+++ Homework3/educational/system/Course.java
@@ -0,0 +1,76 @@
+package educational.system;
+
+import java.util.Arrays;
+
+public class Course {
+    private String name;
+    private int hours;
+    private Tutor tutor;
+    private Student[] students;
+
+    public Course(String name, int hours, Tutor tutor, Student[] students) {
+        this.name = name;
+        this.hours = hours;
+        this.tutor = tutor;
+        this.students = students;
+    }
+
+    public Course(String name, int hours, Tutor tutor) {
+        this(name, hours, tutor, null);
+    }
+
+    public Course(String name, int hours) {
+        this(name, hours, null, null);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(Tutor tutor) {
+        this.tutor = tutor;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", hours=" + hours +
+                ", tutor=" + tutor +
+                ", students=" + Arrays.toString(students) +
+                '}';
+    }
+
+    public String getStudentInfo() {
+        if(students == null) return null;
+        String result = "";
+        for(Student student: students){
+
+        }
+        return result;
+    }
+}
---
+++ Homework3/educational/system/CourseTutorAutoMatcher.java
@@ -0,0 +1,46 @@
+package educational.system;
+
+import java.util.Random;
+
+public class CourseTutorAutoMatcher {
+    public CourseTutorAutoMatcher(){
+        this.random = new Random();
+    }
+    private Random random;
+
+    public void autoMatch(Course[] courses, Tutor[] tutors) {
+
+        for (Course course: courses) {
+            course.setTutor(tutors[random.nextInt(tutors.length)]);
+            if (course.getTutor().getCourses() == null){
+                Course courseListToSet[] = new Course[courses.length];
+                courseListToSet[0] = course;
+                course.getTutor().setCourses(courseListToSet);
+            } else{
+                Course[] courseArray = course.getTutor().getCourses();
+                boolean wasSet = false;
+                for (int i = 0; i < courseArray.length; i++) {
+                    if(courseArray[i] == null){
+                        courseArray[i] = course;
+                        wasSet = true;
+                        break;
+                    }
+                }
+                if (!wasSet){
+                    Course[] extendedCourseList = getExtendedCourseList(courseArray);
+                    extendedCourseList[courses.length] = course;
+                    course.getTutor().setCourses(extendedCourseList);
+                }
+            }
+        }
+    }
+
+    private Course[] getExtendedCourseList(Course[] courseArray) {
+        Course[] resultExtendedCourseList = new Course[courseArray.length * 2];
+        for (int i = 0; i < courseArray.length; i++) {
+            resultExtendedCourseList[i] = courseArray[i];
+
+        }
+        return resultExtendedCourseList;
+    }
+}
