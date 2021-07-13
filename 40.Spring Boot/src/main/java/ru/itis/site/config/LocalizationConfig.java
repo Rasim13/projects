896612153ea95938e23af4c562c7b19c144019b5
Rasim <Rasim467@mail.ru>
2021-07-13 21:23:23 +0400

    add localization

---
+++ 40.Spring Boot/src/main/java/ru/itis/site/config/LocalizationConfig.java
@@ -0,0 +1,63 @@
+package ru.itis.site.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class LocalizationConfig implements WebMvcConfigurer {
+
+    // нужен для обработки специальных ошибок
+    @Override
+    public MessageCodesResolver getMessageCodesResolver() {
+        DefaultMessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+        codesResolver.setMessageCodeFormatter(DefaultMessageCodesResolver.Format.POSTFIX_ERROR_CODE);
+        return codesResolver;
+    }
+
+    // регистрируем наш перехватчик локализации
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    // нам неохдимо запомнить, какой язык выбрал пользователь
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setCookieName("locale");
+        localeResolver.setCookieMaxAge(60 * 60 * 24 * 365);
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        return localeResolver;
+    }
+
+    // отслеживает, когда нужно сменить язык
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        // сменить язык нужно тогда, когда направляется запрос с параметром GET /signUp?lang=ru
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    // указывае путь откуда брать файлы messages
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
