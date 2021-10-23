cfeee2d9c76794742fdc5ff7f6bd99c71d859864
Rasim <Rasim467@mail.ru>
2021-10-23 18:43:12 +0400

    add lambda expression

---
+++ Lambda-expressions/src/main/java/lambda/Student.java
@@ -0,0 +1,50 @@
+package lambda;
+
+public class Student {
+
+    private String name;
+    private char sex;
+    private int age;
+    private int course;
+    private double avgGrade;
+
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public double getAvgGrade() {
+        return avgGrade;
+    }
+}
---
+++ Lambda-expressions/src/main/java/lambda/StudentInfo.java
@@ -0,0 +1,56 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentInfo {
+
+    void testStudents(List<Student> al, StudentChek sc) {
+        for (Student s : al) {
+            if (sc.check(s)) {
+                System.out.println(s);
+            }
+        }
+
+    }
+
+}
+    class TestStudent {
+        public static void main(String[] args) {
+            Student st1 = new Student("Ivan", 'm', 22, 3, 8.3);
+            Student st2 = new Student("Nikolay", 'm', 28, 2, 6.4);
+            Student st3 = new Student("Elena", 'f', 19, 1, 8.9);
+            Student st4 = new Student("Petr", 'm', 35, 4, 7);
+            Student st5 = new Student("Mariya", 'f', 23, 3, 9.1);
+
+            List<Student> students = new ArrayList<>();
+            students.add(st1);
+            students.add(st2);
+            students.add(st3);
+            students.add(st4);
+            students.add(st5);
+
+            StudentInfo info = new StudentInfo();
+
+            info.testStudents(students, p ->
+                 p.getAvgGrade() > 8);
+
+            System.out.println("--------------------------------");
+            info.testStudents(students, (Student s) -> {
+                return s.getAge() < 30;
+            });
+            System.out.println("--------------------------------");
+            info.testStudents(students, (Student s) -> {
+                return s.getAge() > 20 && s.getAvgGrade() < 9.3 && s.getSex() == 'f';
+            });
+        }
+    }
+
+interface StudentChek {
+    boolean check(Student s);
+}
+
+
+
+
+
